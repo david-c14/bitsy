424670b03b2f3766baff7a1e9410a2dfafb023f1
Adam Le Doux <adamrossledoux@gmail.com>
2021-02-16 11:44:33 -0800

    initial stubs

---
+++ editor/script/system/soundchip.js
@@ -0,0 +1,23 @@
+/*
+TODO / NOTES
+- what should the name of the parent directory be: system, hardware, api?
+- square wave channel
+- square wave properties
+	- frequency
+	- duty
+	- length
+	- volume?
+- how should duty options be represented?
+- what do I do about the global state in here?
+*/
+
+
+// todo : name? boot, init, start
+void initSound() {
+
+}
+
+// todo : how many channels? how should they be named?
+void setSoundChannel(frequency) {
+
+}
