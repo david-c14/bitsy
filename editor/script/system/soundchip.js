7d27e5df1faec4a7cefb05a7c8e62fc031683ca6
Adam Le Doux <adamrossledoux@gmail.com>
2021-02-16 12:25:04 -0800

    separate settings and playback control

--- editor/script/system/soundchip.js
+++ editor/script/system/soundchip.js
@@ -3,12 +3,13 @@ TODO / NOTES
 - what should the name of the parent directory be: system, hardware, api?
 - square wave channel
 - square wave properties
-	- frequency
-	- duty
+	X frequency
+	- duty (currently stuck at 50%)
 	- length
 	- volume?
 - how should duty options be represented?
 - what do I do about the global state in here?
+- allow changes to duty (aka "pulse width") of square wave
 */
 
 var audioContext;
@@ -22,10 +23,13 @@ function initSound() {
 	oscillator.start();
 }
 
+function setSoundChannel(freq) {
+	oscillator.frequency.setValueAtTime(freq, audioContext.currentTime); // frequency in hertz
+}
+
 // todo : how many channels? how should they be named?
 // todo : what should length units be? seconds? ms?
-function playSoundChannel(freq, len) {
-	oscillator.frequency.setValueAtTime(freq, audioContext.currentTime); // frequency in hertz
+function playSoundChannel(len) {
 	oscillator.connect(audioContext.destination);
 
 	if (len) {
