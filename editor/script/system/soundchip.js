893a692bc35a37c048e3ac82fec163a7713e4dbf
Adam Le Doux <adamrossledoux@gmail.com>
2021-02-16 12:05:13 -0800

    basic square wave

--- editor/script/system/soundchip.js
+++ editor/script/system/soundchip.js
@@ -11,13 +11,28 @@ TODO / NOTES
 - what do I do about the global state in here?
 */
 
+var audioContext;
+var oscillator;
 
 // todo : name? boot, init, start
-void initSound() {
-
+function initSound() {
+	audioContext = new AudioContext();
+	oscillator = audioContext.createOscillator();
+	oscillator.type = "square";
+	oscillator.start();
 }
 
 // todo : how many channels? how should they be named?
-void setSoundChannel(frequency) {
+// todo : what should length units be? seconds? ms?
+function playSoundChannel(freq, len) {
+	oscillator.frequency.setValueAtTime(freq, audioContext.currentTime); // frequency in hertz
+	oscillator.connect(audioContext.destination);
 
+	if (len) {
+		setTimeout(stopSoundChannel, len);
+	}
+}
+
+function stopSoundChannel() {
+	oscillator.disconnect();
 }
