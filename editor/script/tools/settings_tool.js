8392ea252864d7e3cd6221defe42e84cceb68c9f
Adam Le Doux <adamrossledoux@gmail.com>
2021-03-15 18:45:01 -0700

    stub of new settings

--- editor/script/tools/settings_tool.js
+++ editor/script/tools/settings_tool.js
@@ -4,6 +4,7 @@ installCard(function(card) {
 	card.sizeHint = "M";
 
 	var curSettingsTab = "publish";
+	var curPageSizeMode = "full";
 
 	card.menu = function() {
 		menu.add({
@@ -18,17 +19,89 @@ installCard(function(card) {
 			],
 		});
 
-		if (curSettingsTab === "editor") {
+		if (curSettingsTab === "publish") {
+			menu.add({
+				control: "label",
+				text: "html page color:",
+			});
+
+			menu.add({
+				control: "label",
+				text: "<insert color controls>",
+			});
+
+			menu.add({
+				control: "label",
+				text: "game window size:",
+			});
+
+			menu.add({
+				control: "tabs",
+				name: "gameWindowMode",
+				value: curPageSizeMode,
+				event: "changePageSizeMode",
+				tabs: [
+					{ text: "full page", icon: "pagesize_full", value: "full", },
+					{ text: "fixed size", icon: "pagesize_fixed", value: "fixed", },
+				],
+			});
+
+			if (curPageSizeMode === "fixed") {
+				menu.startGroup();
+
+				menu.add({
+					control: "number",
+				});
+
+				menu.add({
+					control: "label",
+					text: "px",
+				});
+
+				menu.endGroup();
+			}
+		}
+		else if (curSettingsTab === "editor") {
+			menu.startGroup();
+
 			menu.add({
 				control: "label",
 				text: "language:",
 			});
+
+			menu.add({
+				control: "label",
+				text: "<insert language select>",
+			});
+
+			menu.endGroup();
+
+			menu.add({
+				control: "label",
+				text: "<insert langauge credit>",
+			});
 		}
 		else if (curSettingsTab === "system") {
+			menu.startGroup();
+
 			menu.add({
 				control: "label",
 				text: "font:",
 			});
+
+			menu.add({
+				control: "label",
+				text: "<insert font select>",
+			});
+
+			menu.endGroup();
+
+			menu.add({
+				control: "label",
+				text: "<insert font description>",
+			});
+
+			menu.startGroup();
 
 			menu.add({
 				control: "label",
@@ -37,23 +110,33 @@ installCard(function(card) {
 
 			menu.add({
 				control: "label",
-				text: "animation:",
+				text: "<insert text direction select>",
 			});
-		}
-		else if (curSettingsTab === "publish") {
+
+			menu.endGroup();
+
 			menu.add({
 				control: "label",
-				text: "html page color:",
+				text: "(option for languages that read right to left)",
 			});
 
 			menu.add({
 				control: "label",
-				text: "game window size:",
+				text: "animation:",
+			});
+
+			menu.add({
+				control: "label",
+				text: "<TODO: animation settings>",
 			});
 		}
 	};
 
 	card.changeSettingsTab = function(value) {
 		curSettingsTab = value;
+	};
+
+	card.changePageSizeMode = function(value) {
+		curPageSizeMode = value;
 	};
 });
