71495438fcc31777f4c7b67628d37e55ccf46674
Adam Le Doux <adamrossledoux@gmail.com>
2021-04-10 11:57:07 -0700

    install and cache resources

--- editor/app.js
+++ editor/app.js
@@ -2,7 +2,15 @@ if ("serviceWorker" in navigator) {
 	console.log("service workers exist :)");
 
 	navigator.serviceWorker.register("sw.js").then(function (reg){
-		console.log("service worker registered!");
+		if (reg.installing) {
+			console.log("service worker installing...");
+		}
+		else if (reg.waiting) {
+			console.log("service worker installed!");
+		}
+		else if (reg.active) {
+			console.log("service worker active :)");
+		}
 	}).catch(function(error) {
 		console.log("service worker registration failed :(");
 		console.log("service worker error: " + error);
--- editor/sw.js
+++ editor/sw.js
@@ -3,5 +3,51 @@ SERVICE WORKER
 
 TODO / NOTES:
 - is the root directory the right place for this?
-
+- wow I really need to clean up and re-organize my scripts..
 */
+
+var cacheVersionId = "bitsy-cache-v1";
+
+self.addEventListener("install", function(event) {
+	event.waitUntil(
+		caches.open(cacheVersionId).then(function(cache) {
+			return cache.addAll([
+				"manifest.json",
+				"index.html",
+				"app.js",
+				"image/cat5.png",
+				"/style/bitsyStyle.css",
+				"/style/editorAnimations.css",
+				"/script/generated/resources.js",
+				"/script/engine/color_util.js",
+				"/script/engine/font.js",
+				"/script/engine/transition.js",
+				"/script/engine/script.js",
+				"/script/engine/dialog.js",
+				"/script/engine/renderer.js",
+				"/script/engine/bitsy.js",
+				"/script/shared/gfx.js",
+				"/script/shared/card.js",
+				"/script/shared/card_ui.js",
+				"/script/tools/settings_tool.js",
+				"/script/tools/paint_tool.js",
+				"/script/util.js",
+				"/script/icons.js",
+				"/script/paint.js",
+				"/script/room.js",
+				"/script/room_markers.js",
+				"/script/explorer.js",
+				"/script/localization.js",
+				"/script/event_manager.js",
+				"/script/palette.js",
+				"/script/color_picker.js",
+				"/script/gif.js",
+				"/script/exporter.js",
+				"/script/dialog_editor.js",
+				"/script/editor.js",
+				"/script/inventory.js",
+				"/style/googleNunito.css",
+			]);
+		})
+	);
+});
