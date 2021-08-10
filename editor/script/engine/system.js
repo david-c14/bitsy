3434ff666c452ea699c84c91458f677923be3e35
Adam Le Doux <adamrossledoux@gmail.com>
2021-08-10 07:40:59 -0700

    experimenting with more complex stuff

--- editor/script/engine/system.js
+++ editor/script/engine/system.js
@@ -17,15 +17,35 @@ function bitsyLog(message, category) {
 /* SOUND */
 var audioContext;
 var oscillator;
+var threshold;
 
 // init audio context and square wave oscillator
 audioContext = new AudioContext();
+
 oscillator = audioContext.createOscillator();
 oscillator.type = "square";
+
+// experimenting with more complex stuff
+// threshold = audioContext.createWaveShaper();
+// var curve = new Float32Array(8);
+// curve[0] = -1;
+// curve[1] = -1;
+// curve[2] = -1;
+// curve[3] = -1;
+// curve[4] = 1;
+// curve[5] = 1;
+// curve[6] = 1;
+// curve[7] = 1;
+// threshold.curve = curve;
+// oscillator.connect(threshold);
+
 oscillator.start();
 
+var inputNode = oscillator;
+var outputNode = oscillator;
+
 function bitsyPlayNote(frequency, length) {
-	oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime); // frequency in hertz
-	oscillator.connect(audioContext.destination);
-	setTimeout(function() { oscillator.disconnect(); }, length);
+	inputNode.frequency.setValueAtTime(frequency, audioContext.currentTime); // frequency in hertz
+	outputNode.connect(audioContext.destination);
+	setTimeout(function() { outputNode.disconnect(); }, length);
 }
